05be5eceab1bfeddda36272a0d3b5daacab2a3af
Aline <128093293+Alinedev85@users.noreply.github.com>
2023-07-09 00:00:18 -0300

    Cronologia das postagens

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,46 +1,40 @@
 import './feed.css';
 import { publicações, retornoPublicacoes } from '../../configFirebase/post.js';
 
-
 export default () => {
   const container = document.createElement('div');
 
   const template = `
   <div class="container">
-
-      <div class="logo"> 
+    <div class="logo"> 
       <img src="./images/logo5.png">
-      </div>
+    </div>
 
-        <section class="menu">
-         <h2 class="saudacao">Olá, <span id="nome-usuario">fulaninho(a)</span>!</h2>
-         <h3 class="convite">Possui um convite? Acesse:</h3>
-         <ul>
-           <li><a href="#cafeComRum">Café com Rum</a></li>
-         </ul>
-          <button id="logoutButton" class="logout">Deslogar</button>
-        </section>
+    <section class="menu">
+      <h2 class="saudacao">Olá, <span id="nome-usuario">fulaninho(a)</span>!</h2>
+      <h3 class="convite">Possui um convite? Acesse:</h3>
+      <ul>
+        <li><a href="#cafeComRum">Café com Rum</a></li>
+      </ul>
+      <button id="logoutButton" class="logout">Deslogar</button>
+    </section>
         
-        <section class="filtros">
-         <h2 class="saudacao">Acesse também:</h2>
-         <ul>
-           <li><a href="https://www.conab.gov.br/" target="_blank">Atualizações da Conab.gov.br</a></li>
-           <li><a href="https://app.powerbi.com/view?r=eyJrIjoiNDdkNDM4ZjctYzk0OS00NWVjLWFlYjktZWQ4Njg3MDEyMTg0IiwidCI6ImU2ZDkwZGYzLWYxOGItNGJkZC04MDhjLWFhNmQwZjY4YjgwOSJ9" target="_blank">Busca por Armazenadores</a></li>
-            <li><a href="https://portaldeinformacoes.conab.gov.br/produtos-360.html" target="_blank">Acompanhe o preço da saca</a></li>
-          </ul>
-          <img class="fundo" src="./images/background-filtros.png">
-        </section>
-     
-       
-        <div class="postagens">
-         <textarea class="inputMensagem" id="areaMensagem" placeholder="Compartilhe ideias e informações sobre café"></textarea>
-         <button class="btnPostagem" id="postagemID">Postar</button>
-         </div>
+    <section class="filtros">
+      <h2 class="saudacao">Acesse também:</h2>
+      <ul>
+        <li><a href="https://www.conab.gov.br/" target="_blank">Atualizações da Conab.gov.br</a></li>
+        <li><a href="https://app.powerbi.com/view?r=eyJrIjoiNDdkNDM4ZjctYzk0OS00NWVjLWFlYjktZWQ4Njg3MDEyMTg0IiwidCI6ImU2ZDkwZGYzLWYxOGItNGJkZC04MDhjLWFhNmQwZjY4YjgwOSJ9" target="_blank">Busca por Armazenadores</a></li>
+        <li><a href="https://portaldeinformacoes.conab.gov.br/produtos-360.html" target="_blank">Acompanhe o preço da saca</a></li>
+      </ul>
+      <img class="fundo" src="./images/background-filtros.png">
+    </section>
 
-         <section id="postagem" class="postagemFeed">
-         </section> 
+    <div class="postagens">
+      <textarea class="inputMensagem" id="areaMensagem" placeholder="Compartilhe ideias e informações sobre café"></textarea>
+      <button class="btnPostagem" id="postagemID">Postar</button>
+    </div>
 
-
+    <section id="postagem" class="postagemFeed"></section> 
   `;
 
   container.innerHTML = template;
@@ -53,7 +47,7 @@ export default () => {
     if (mensagem.length > 0) {
       await publicações(mensagem);
       container.querySelector('#areaMensagem').value = '';
-      mostrarPostagem();
+      await mostrarPostagem();
     } else {
       alert('Digite sua mensagem!');
     }
@@ -64,29 +58,40 @@ export default () => {
     const postagem = container.querySelector('#postagem');
     postagem.innerHTML = "";
 
-    if (publicacoes.length > 0) {
-      publicacoes.forEach((post) => {
+    // NaN = "Not-a-Number"
+    const postagensValidas = publicacoes.filter((post) => !isNaN(post.timestamp));
+
+    postagensValidas.sort((a, b) => b.timestamp - a.timestamp);
+
+    if (postagensValidas.length > 0) {
+      postagensValidas.forEach((post) => {
         const publicar = document.createElement('div');
+
+        const timestamp = post.timestamp;
+        const date = new Date(timestamp);
+        const dataFormatada = date.toLocaleString();
+
         publicar.innerHTML = ` 
           <section class='conteudo'>
-            <h3 class='nome'> ${post.name}</h3>
+            <div class='nome-data'>
+              <h3 class='nome'> ${post.name}</h3>
+              <p class='timestamp'> ${dataFormatada}</p>
+            </div>
             <p class='conteudoPag'> ${post.msg}</p>
             <div class='botoes'>
               <button class='botaoCurtir'>Curtir</button>
-              <button class='botaoComentar'>Comentar</button>
+              <button class='botaoExtra'>btn extra</button>
             </div>
-          </section> `;
+          </section>`;
 
         postagem.appendChild(publicar);
       });
     }
 
-  };
 
-
+  }
 
   mostrarPostagem();
 
   return container;
-
 };
